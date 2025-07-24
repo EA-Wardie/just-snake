6f0a9091fdbc49eb69ecb0bdff83ff3775b601c4
Edward van Tonder <56725833+EA-Wardie@users.noreply.github.com>
2025-07-24 23:59:59 +0200

    WIP

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,6 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<main class="h-dvh w-dvw overflow-hidden bg-neutral-950">
+	{@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	const snake = $state([
+		[16, 11],
+		[16, 12],
+		[16, 13],
+		[16, 14],
+		[16, 15],
+	]);
+
+	const isSnakePart = (index: number) => {
+		return snake.some((part) => part.includes(index));
+	};
+</script>
+
+<section class="mx-auto grid aspect-square h-full grid-cols-32">
+	{#each { length: 32 * 32 }, index}
+		<div class="border border-white bg-{isSnakePart(index) ? 'white' : ''}"></div>
+	{/each}
+</section>
