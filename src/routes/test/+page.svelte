2880e8a47b2a63078b47464642b4ef357b067e56
Edward van Tonder <56725833+EA-Wardie@users.noreply.github.com>
2025-07-29 22:53:15 +0200

    Segment direction && keyboard events WIP

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Coordinates, Direction, Segment } from '$lib';
+	import { Direction, Segment } from '$lib';
 
 	const cellSize = 32;
 	const columns = innerWidth / cellSize;
@@ -14,68 +14,100 @@
 		new Segment(startX, startY + 3),
 	]);
 
-	let gameIndex = $state(0);
-
+	let segmentIndex = 0;
 	let interval: number;
-
-	const move = () => {
-		snake.forEach((segment) => segment.move());
-	};
-
-	const setSegmentDirection = (segment: Segment, direction: Direction) => {
-		segment.setDirection(direction);
-	};
+	let directions: Direction[] = [];
 
 	const getSegmentColor = (index: number) => {
 		return index === 0 ? 'bg-amber-600' : 'bg-primary';
 	};
 
-	const getSegmentRadius = (index: number) => {
-		const segment = snake[index];
-		const isHead = index === 0;
-		const isTail = index === snake.length - 1;
+	const isInBounds = () => {
+		const head = snake.at(0);
 
-		if (segment.getDirection() === Direction.UP && isHead) {
-			return 'rounded-t-2xl rounded-b-md';
-		} else if (segment.getDirection() === Direction.RIGHT && isHead) {
-			return 'rounded-r-2xl rounded-l-md';
+		if (head) {
+			return (
+				head.getX() > -1 && head.getY() > -1 && head.getX() < columns - 1 && head.getY() < rows - 1
+			);
 		}
 
-		return 'rounded-md';
+		return false;
+	};
+
+	const checkAndUpdateDirection = () => {
+		const newDirection = directions.at(0);
+
+		if (newDirection) {
+			snake[segmentIndex].setDirection(directions[segmentIndex]);
+			segmentIndex++;
+		}
+
+		if (segmentIndex === snake.length) {
+			directions.shift();
+			segmentIndex = 0;
+		}
+	};
+
+	const moveSnake = () => {
+		snake.forEach((segment) => segment.move());
 	};
 
 	const initGameLoop = () => {
-		let segmentIndex = 0;
-
 		clearInterval(interval);
 
 		interval = setInterval(() => {
-			if (gameIndex < 10) {
-				if (gameIndex > 3 && segmentIndex < snake.length) {
-					setSegmentDirection(snake[segmentIndex], Direction.RIGHT);
-					segmentIndex++;
-				} else {
-					segmentIndex = 0;
-				}
-
-				gameIndex++;
-
-				move();
+			if (isInBounds()) {
+				checkAndUpdateDirection();
+				moveSnake();
 			} else {
 				clearInterval(interval);
 			}
-		}, 500);
+		}, 300);
 	};
 
 	initGameLoop();
+
+	const updateDirectionInput = (event: KeyboardEvent) => {
+		switch (event.key) {
+			case 'ArrowUp':
+				if (directions.at(-1) !== Direction.DOWN) {
+					directions.push(Direction.UP);
+				}
+
+				break;
+			case 'ArrowDown':
+				if (directions.at(-1) !== Direction.UP) {
+					directions.push(Direction.DOWN);
+				}
+
+				break;
+			case 'ArrowLeft':
+				if (directions.at(-1) !== Direction.RIGHT) {
+					directions.push(Direction.LEFT);
+				}
+
+				break;
+			case 'ArrowRight':
+				if (directions.at(-1) !== Direction.LEFT) {
+					directions.push(Direction.RIGHT);
+				}
+
+				break;
+		}
+	};
+
+	const registerKeyboardEvents = () => {
+		removeEventListener('keyup', updateDirectionInput);
+		addEventListener('keyup', updateDirectionInput);
+	};
+
+	registerKeyboardEvents();
 </script>
 
 <section class="relative h-full w-full border-8 border-neutral-600 text-white">
 	{#each snake as segment, index}
 		<div
-			class="absolute aspect-square transition-all duration-100 {getSegmentColor(
-				index,
-			)} {getSegmentRadius(index)}"
+			class="absolute aspect-square transition-all duration-100 {getSegmentColor(index)}"
 			style="width: {cellSize}px; left: {segment.getX() * cellSize}px; top: {segment.getY() *
 				cellSize}px"
 		></div>
